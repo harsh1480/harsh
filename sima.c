7a66aba8cd67a9cafc1f7edd16cad54afaa7e3c4
harsh1480 <90342113+harsh1480@users.noreply.github.com>
2022-01-15 16:27:43 +0530

    Add files via upload

---
+++ sima.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+ int i=0,j=0,n;
+ void *p;
+ char ch, b[15],c,sp;
+ printf("Expression terminated by $:");     
+ while((c=getchar())!='$')
+ {	
+  b[i]=c;                                        
+  i++;
+ }
+ n=i-1;                                   
+ printf("Given Expression:");
+ i=0;
+ while(i<=n)
+ {
+  printf("%c",b[i]);                  
+  i++;
+ }
+ printf("\n Symbol Table\n");
+ printf("Symbol \t addr \t type");
+ while(j<=n)
+ {
+  c=b[j];                    
+  
+  if(isalpha(toascii(c)))
+  {
+   p=malloc(c);
+   printf("\n%c \t %d \t identifier\n",c,p);
+  }
+else if(c == '!' || c == '@' || c == '#' ||  c == '%' || c == '&' || c == '?' || c == '/')
+  {
+      p = malloc(c);
+      printf("\n %c \t %d \t Special Operator\n",c,p);
+  }
+  else
+  {
+   ch=c;
+   if(ch=='+'||ch=='-'||ch=='*'||ch=='=')
+   {
+    p=malloc(ch);
+     printf("\n %c \t %d \t operator\n",ch,p);
+   } 
+      
+  }
+
+  j++;
+     
+ }
+    
+}
